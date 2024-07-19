dc9d1bbb30bb5481c4d99b1204e37c0a95fb0058
eitan-spitz <eitands@gmail.com>
2024-07-19 20:35:26 +0700

    adding module

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,3 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.42.0.0/16"
+
+  azs             = ["us-east-1a"]
+  private_subnets = ["10.42.1.0/24"]
+  public_subnets  = ["10.42.2.0/24"]
+
+  tags = {
+    Terraform = "true"
+    DevopsMaster = "true"
+  }
+}
